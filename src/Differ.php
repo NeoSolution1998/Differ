2c6d5508b6e8f750eb4fe4c32131866ac202a4b8
neosolution <neosolution1998@gmail.com>
2021-11-09 12:47:49 +0300

    6 шаг. Add diffTree

--- src/Differ.php
+++ src/Differ.php
@@ -7,53 +7,51 @@ use Docopt;
 use function Funct\Collection\union;
 use function Differ\parsers\parse;
 
-function genDiff($pathToFile1, $pathToFile2)
-{
+function genDiff($pathToFile1, $pathToFile2) 
+{	
     $beforeFile = file_get_contents($pathToFile1);
     $afterFile = file_get_contents($pathToFile2);
-    echo " Данные из файла 1\n";
-    print_r($beforeFile);
-    echo "\n";
+    
     $format1 = pathinfo($pathToFile1, PATHINFO_EXTENSION);
     $format2 = pathinfo($pathToFile2, PATHINFO_EXTENSION);
 
-    echo  "Формат из файла 1\n";
-    print_r($format1);
-    echo  "\n";
-    echo "\n";
-    $fileParse1 = parse($beforeFile, $format1);
-    $fileParse2 = parse($afterFile, $format2);
+    $data1 = parse($beforeFile, $format1);
+    $data2 = parse($afterFile, $format2);
 
-    echo "ФАЙЛ 1 ПОСЛЕ ПАРСИНГА \n";
-    print_r($fileParse1);
+    $tree = buildDiff($data1, $data2);
+    return $tree;
+}
 
-    $file1 = (array) $fileParse1;
-    $file2 = (array) $fileParse2;
-    echo "ФАЙЛ 1 ПОСЛЕ ARRAY \n";
-    print_r($file1);
+    function node (string $key, string $type, $oldValue, $newValue, array $children = [])
+    {
+    return [
+            'key' => $key,
+            'type' => $type,
+           'oldValue' => $oldValue,
+            'newValue' => $newValue,
+            'children' => $children,
+        ];
+    }
 
-    $keys1 = array_keys((array)$file1);
-    $keys2 = array_keys((array)$file2);
+function buildDiff($data1, $data2)
+{
+    $keys1 = array_keys((array) $data1);
+    $keys2 =  array_keys((array) $data2);
     $keys = union($keys1, $keys2);
-    echo "Ключи общие";
-    print_r($keys);
-    return array_reduce(
-        $keys,
-        function ($acc, $key) use ($file1, $file2) {
-            if (array_key_exists($key, $file1) && array_key_exists($key, $file2)) {
-                if ($file1[$key] === $file2[$key]) {
-                        $result = "{$key} {$file2[$key]}\n";
-                } else {
-                        $result = "- {$key} {$file1[$key]}\n";
-                        $result .= "+ {$key} {$file2[$key]}\n";
-                }
-            } elseif (array_key_exists($key, $file1) && !array_key_exists($key, $file2)) {
-                $result = "- {$key} {$file1[$key]}\n";
-            } else {
-                $result = "+ {$key} {$file2[$key]}\n";
-            }
-            $acc .= $result;
-            return $acc;
-        },
-    );
+    sort($keys);
+    return array_map(function ($key) use ($data1, $data2) {
+        if (!property_exists($data1, $key)) {
+            return node($key, 'added', null, $data2->$key);
+        }
+        if (!property_exists($data2, $key)) {
+            return node($key, 'deleted', $data1->$key, null);
+        }
+        if (is_object($data1->$key) && is_object($data2->$key)) {
+            return node($key, 'nested', null, null, buildDiff($data1->$key, $data2->$key));
+        }
+            if ($data1->$key == $data2->$key) {
+                return node($key, 'unchanged', $data1->$key, null);
+        }
+            return node($key, 'changed', $data1->$key, $data2->$key);
+        }, $keys);
 }
